d0547f5339dd29ed3e6755601380073836ad7710
EBA-PC\EBA <hatakou9169@gmail.com>
2017-08-09 18:43:33 +0900

    githubで管理するためサイトデータを全て移行

---
+++ js/init_pc.js
@@ -0,0 +1,22 @@
+(function($){
+$(function(){
+
+//---------------------------------------------------------------------------------------------- http://semooh.jp/jquery/cont/doc/easing/
+
+	$('a,.submit_area input').not('#gnav a,#reason li a').hover(function(){
+			$(this).stop(true,true).animate({opacity: 0.6},200, 'easeOutQuart');
+		}, function(){
+			$(this).stop(true,true).animate({opacity: 1.0},400,'easeOutQuart');
+			return false;
+	});
+
+	$('#mainnav a').not('#mainnav a.stay').hover(function(){
+			$(this).stop(true,true).animate({backgroundColor: '#4A9ED3'},200, 'easeOutQuart');
+		}, function(){
+			$(this).stop(true,true).animate({backgroundColor: '#2980b9'},400,'easeOutQuart');
+			return false;
+	});
+
+
+});
+})(jQuery);
